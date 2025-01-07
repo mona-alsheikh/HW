8dd1f95e7f78fcb0649b9adc5938cf76d7fcc868
mona-alsheikh <mny256182@gmail.com>
2025-01-08 00:44:58 +0200

    comit

--- home5.c
+++ home5.c
@@ -1,8 +1,11 @@
+
 #include <stdio.h>
 int main() {
-    for(int i=0;i<5;i++){
-        printf("#")
+    for(int i=0;i<10;i++){
+        if(i%2==0)
+        printf("even\n");
+    else
+    printf("odd\n");
     }
-        
     return(0);
 }
