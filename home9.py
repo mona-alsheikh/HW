2d65ece616719f91ba74db4e052312ffe27698c0
mona-alsheikh <mny256182@gmail.com>
2025-01-13 02:01:42 +0200

    comit

---
+++ home9.py
@@ -0,0 +1,8 @@
+numbers = [-1,0,7,-1,6,-4,8,0]
+for number in numbers:
+    if number > 0:
+        print(f"positive")
+    elif number < 0:
+        print("negative")
+    else:
+        print(f"number is zero")
