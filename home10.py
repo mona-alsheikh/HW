5a1ea12c7fda40e969d75fe9d833beb7239dd815
mona-alsheikh <mny256182@gmail.com>
2025-01-18 17:55:38 +0200

    comit

---
+++ home10.py
@@ -0,0 +1,9 @@
+i = 1
+while i <= 12:
+    if i == 1 or i == 2 or i == 4:
+        print(" ",end="")
+    else:
+        print("1",end="")
+    if i == 3 or i == 7:
+        print()        
+    i = i+1
